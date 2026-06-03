de3408f8e29ec4b59be3dde5ae27a232f35cded1
mhmd aminuddin <dindhyn18@gmail.com>
2026-06-04 02:31:54 +0700

    Fix duplicate footer and navbar on public pages

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,14 +1,7 @@
-import Navbar from './Navbar';
-import Footer from './Footer';
-
 export default function Layout({ children }) {
   return (
-    <div className="min-h-screen flex flex-col font-sans">
-      <Navbar />
-      <main className="flex-grow pt-20">
-        {children}
-      </main>
-      <Footer />
+    <div className="pt-20">
+      {children}
     </div>
   );
 }
