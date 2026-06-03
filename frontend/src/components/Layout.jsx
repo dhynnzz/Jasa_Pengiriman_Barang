ed23d0f280102cfa7fc7884d3ba3f240b27cf0ce
mhmd aminuddin <dindhyn18@gmail.com>
2026-06-03 22:48:34 +0700

    Fix navbar overlapping page content by adding pt-20 to main layout

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -3,14 +3,12 @@ import Footer from './Footer';
 
 export default function Layout({ children }) {
   return (
-    <div className="min-h-screen flex flex-col font-sans relative overflow-x-hidden">
+    <div className="min-h-screen flex flex-col font-sans">
       <Navbar />
-      <main className="flex-grow w-full flex flex-col relative z-10">
+      <main className="flex-grow pt-20">
         {children}
       </main>
-      <div className="mt-auto relative z-20 w-full bg-white">
-        <Footer />
-      </div>
+      <Footer />
     </div>
   );
 }
